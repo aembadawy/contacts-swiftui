af2c70b1e89e390a8fed9c06100ddd107d74d5a2
aembadawy <aem.badawy@gmail.com>
2025-04-02 23:00:29 +0200

    Initial Commit

---
+++ contacts-swiftui/contacts_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  contacts_swiftuiApp.swift
+//  contacts-swiftui
+//
+//  Created by Aya on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct contacts_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
