f8e19d3a671f239e5938232df3ed5fe4095073ba
aembadawy <aem.badawy@gmail.com>
2025-04-02 23:36:01 +0200

    Add static contacts with list display

--- contacts-swiftui/ContactsApp/View/ContactsView.swift
+++ contacts-swiftui/ContactsApp/View/ContactsView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct ContactsView: View {
+    @State private var contacts: [Contact] = [
+        .init(id: "1",
+              firstName: "Pierre",
+              lastName: "Gasly",
+              eamil: "p.gasly@f1.com"),
+        .init(id: "2",
+              firstName: "Max",
+              lastName: " Verstappen",
+              eamil: "m.verstappen@f1.com"),
+        .init(id: "3",
+              firstName: "Lewis",
+              lastName: "Hamilton",
+              eamil: "l.hamilton@f1.com"),
+        .init(id: "4",
+              firstName: "Charles",
+              lastName: " Leclerc",
+              eamil: "c.leclerc@f1.com")
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(contacts) { contact in
+                Text(contact.eamil)
+            }
+        }
     }
 }
 
